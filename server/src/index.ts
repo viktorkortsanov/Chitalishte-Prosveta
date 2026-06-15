afe1ec5d4e03d098fe8870213220bdf896fc5815
Viktor Kortsanov <viktorkortsanov@gmail.com>
2026-06-15 18:54:49 +0300

    Add authentication middleware

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,12 +3,14 @@ import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import routes from './routes.js';
+import { authMiddleware } from "./middlewares/authMiddleware.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
+app.use(authMiddleware);
 app.use(routes);
 
 app.listen(3000, () => console.log('Server is listening on port 3000'))
--- server/src/middlewares/authMiddleware.ts
+++ server/src/middlewares/authMiddleware.ts
@@ -1,4 +1,31 @@
-import { NextFunction } from "express";
+import { NextFunction, Request, Response } from "express";
+import jsonwebtoken, { JwtPayload } from "jsonwebtoken";
+
+declare global {
+    namespace Express {
+        interface Request {
+            user?: JwtPayload | string;
+            isAuthenticated?: boolean;
+        }
+    }
+}
 
 export const authMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+    const token = req.cookies[process.env.AUTH_COOKIE_NAME as string];
+
+    if (!token) {
+        return next();
+    }
+
+    try {
+        const decodedToken = jsonwebtoken.verify(token, process.env.JWT_SECRET as string);
+        req.user = decodedToken;
+        req.isAuthenticated = true;
+
+        res.locals.user = decodedToken;
+        res.locals.isAuthenticated = true;
+        next();
+    } catch (err) {
+        res.clearCookie(process.env.AUTH_COOKIE_NAME as string);
+    }
 }
