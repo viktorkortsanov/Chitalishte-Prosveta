48789f7d0b73408dd89699ba27978cd142ff1424
Viktor Kortsanov <viktorkortsanov@gmail.com>
2026-07-25 16:13:18 +0300

    Add create article component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,19 +3,23 @@ import RegisterForm from "./components/AuthForms/RegisterFrom"
 import VerifyEmail from "./components/AuthForms/VerifyEmail"
 import NavBar from "./components/NavBar/NavBar"
 import MainContent from "./components/MainContent/MainContent"
-import { Routes, Route } from "react-router"
+import { Routes, Route, useNavigate } from "react-router"
 import Footer from "./components/Footer/Footer"
+import ArticleForm from "./components/Articles/CreateArticle/CreateArticle"
 
 function App() {
+  const navigate = useNavigate();
+
   return (
     <>
       <NavBar />
       <Routes>
-        <Route path="/" element={<MainContent />}></Route>
+        <Route path="/" element={<MainContent />} />
         <Route path="/login" element={<LoginForm />} />
         <Route path="/register" element={<RegisterForm />} />
         <Route path="/verify-email" element={<VerifyEmail />} />
-      </Routes >
+        <Route path="/novini-i-sаbitiya" element={<ArticleForm onClose={() => navigate(-1)} onSubmit={(data) => console.log(data)} />} />
+      </Routes>
       <Footer />
     </>
   )
